3fb7118d6bbd24e939729598f468f2f03655b22c
Eto12 <reverso12yt@gmail.com>
2025-03-23 23:43:15 -0300

    a

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-function createuser(){
-    username = document.getElementById("userinpt").value
-    email = document.getElementById("emailinp").value
-    senha = document.getElementById("senhainp").value
+function createuser() {
+    const username = document.getElementById("userinpt").value;
+    const email = document.getElementById("emailinp").value;
+    const senha = document.getElementById("senhainp").value;
 
-    alert("Usuario "+username +" criado com sucesso!")
+    fetch("http://localhost:3008/create", { // Corrigida a URL
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            username: username,
+            email: email,
+            password: senha
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        alert("Usuário " + data.username + " criado com sucesso!");
+    })
+    .catch(error => {
+        console.error("Erro ao criar usuário:", error);
+        alert("Erro ao criar usuário.");
+    });
 }
