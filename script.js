001eb1bfc1f5b0214b680f32d0d9a9ad3a55769a
Eto12 <reverso12yt@gmail.com>
2025-03-23 23:25:22 -0300

    a

---
+++ script.js
@@ -0,0 +1,7 @@
+function createuser(){
+    username = document.getElementById("userinpt").value
+    email = document.getElementById("emailinp").value
+    senha = document.getElementById("senhainp").value
+
+    alert("Usuario "+username +" criado com sucesso!")
+}
