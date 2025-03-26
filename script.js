0c3f824721af26b493063af4ed44c40faab97b2c
Eto12 <reverso12yt@gmail.com>
2025-03-25 22:48:57 -0300

    a

--- script.js
+++ script.js
@@ -1,25 +1,31 @@
-function createuser() {
+async function createuser() {
     const username = document.getElementById("userinpt").value;
     const email = document.getElementById("emailinp").value;
     const senha = document.getElementById("senhainp").value;
 
-    fetch("http://localhost:3008/create", { // Corrigida a URL
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            username: username,
-            email: email,
-            password: senha
-        })
-    })
-    .then(response => response.json())
-    .then(data => {
+    // Validação simples
+    if (username === "" || email === "" || senha === "") {
+        return alert("Preencha todos os campos!");
+    }
+
+    try {
+        const user = await fetch("http://localhost:3008/create", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ username, email, password: senha })
+        });
+
+        if (!user.ok) {
+            const errorData = await user.json();
+            throw new Error(errorData.error || "Erro na criação do usuário");
+        }
+
+        const data = await user.json();
         alert("Usuário " + data.username + " criado com sucesso!");
-    })
-    .catch(error => {
-        console.error("Erro ao criar usuário:", error);
-        alert("Erro ao criar usuário.");
-    });
+        window.location.href = "login.html"
+    } catch (error) {
+        console.error("Erro:");  // Exibe mensagem de erro caso algo aconteça
+    }
 }
